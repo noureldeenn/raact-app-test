584f49f06e547dabef99eb806bdf97b6c778b841
noureldeenn <nourbadr4646@gmail.com>
2021-11-06 00:47:23 +0200

    fix test

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
+import './App.css';
 import { useEffect, useState } from 'react';
 
 
 function App() {
   const [repositories, setRepositories] = useState(null);
   const [page, setPage] = useState(1);
-
+  console.log(page)
   const api =axios.create({
     baseURL: `https://api.github.com/search/repositories?q=created:%3e2017-10-22&amp;sort=stars&amp;order=desc&page=${page}`
   })
   useEffect(() => {
     api.get('/').then((res) => setRepositories(res.data.items))
-  }, [])
-  console.log(repositories);
+  }, [page])
   return (
-    <div className="App">
-      {repositories?.map((r) => {(
-        <div class="card">
-          <img src={r.owner.avatar_url} alt="Denim Jeans" style="width:100%" />
+    <div>
+      <section >
+        ll
+      {repositories?.map((r) => (
+        <div className="card">
+          <img src={r?.owner?.avatar_url} alt="Denim Jeans" style={{width: "100%" }} />
             <h1>{r.description}</h1>
-            <p class="price">{r.open_issues}</p>
+            <p className="price">{r.open_issues}</p>
             <p>{r.forks}</p>
             <p><button>{r.full_name}</button></p>
         </div>
-        )}
+        )
       )}
-      <button onClick={() => setPage +1}>pagination</button>
+      </section>
+      <button style={{margin: "auto"}} onClick={() => setPage(page + 1)}>pagination</button>
     </div>
   );
 }
